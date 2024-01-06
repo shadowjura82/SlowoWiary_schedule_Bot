dece0a9831ebf5a8d33377b2aefdd3934eabccb7
Yurii <shadowjura82@gmail.com>
2024-01-06 09:45:14 +0200

    Initial commit

---
+++ src/test/java/com/SlowoWiary/scheduleBot/ScheduleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SlowoWiary.scheduleBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
